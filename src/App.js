5132453d39ef3c5605c73556e2085822935e1f51
rabeb-bb <rabeb.bahry@gmail.com>
2021-08-11 11:42:41 +0100

    adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './style.css';
+import imageInSrc from "./imageInSrc.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:"solid 1px black", maxWidth: "100vw"}}>
+        <h1 className="title red">Name here</h1>
+        <br/>
+        <img src={imageInSrc} alt="image in src"></img>
+        <br/>
+        <img src="/imageInPublic.jpg" alt="image in public"></img>
+      </div>
+      <video width='320' height='240' controls>
+        <source src="myVideo.mp4" type="video/mp4"></source>
+      </video>
     </div>
   );
 }
